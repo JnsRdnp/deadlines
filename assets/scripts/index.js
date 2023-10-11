487034804e89a63edb2e1e1e9074e83390f3363b
unknown <joonasroska123@gmail.com>
2023-10-11 23:12:10 +0300

    course class assignments

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,11 +4,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed");
 
     javaCourse = new Course(document);
+    javaCourse.setCourseName("Javascript");
+    javaCourse.setAssignments(87)
+
+    javaCourse.setEndDate("2001-10-10");
+    javaCourse.setStartDate("2002-5-10");
     console.log(javaCourse.getEndDate);
     console.log(javaCourse.getStartDate);
     console.log(javaCourse.getToday);
-
-
+    console.log(javaCourse.getName);
+    console.log(javaCourse.getAssignments);
   });
 
 
@@ -16,32 +21,51 @@ document.addEventListener("DOMContentLoaded", (event) => {
 class Course {
 
     constructor(document) {
-        this.startDate = new Date();
-        this.endDate = new Date();
-        this.today = new Date();
-        this.document = document;
+        this._name = "Course";
+        this._startDate = new Date();
+        this._endDate = new Date();
+        this._today = new Date();
+        this._document = document;
+
+        this._assignments = 0;
+    }
+
+    setCourseName(name){
+        this._name = name;
+    }
+
+    setAssignments(assignments){
+        this._assignments = assignments;
     }
 
     setStartDate(newStartDate){
         // this.startDate = new Date(newStartDate)
-        this.startDate = new Date(newStartDate);
+        this._startDate = new Date(newStartDate);
     }
 
     setEndDate(newEndDate){
         // this.startDate = new Date(newStartDate)
-        this.endDate = new Date(newEndDate);
+        this._endDate = new Date(newEndDate);
+    }
+
+    get getName(){
+        return this._name;
+    }
+
+    get getAssignments(){
+        return this._assignments;
     }
 
     get getStartDate(){
-        return this.getSimpleDate(this.startDate);
+        return this.getSimpleDate(this._startDate);
     }
 
     get getEndDate(){
-        return this.getSimpleDate(this.endDate);
+        return this.getSimpleDate(this._endDate);
     }
 
     get getToday(){
-        return this.getSimpleDate(this.today);
+        return this.getSimpleDate(this._today);
     }
 
     getSimpleDate(inputDate) {
@@ -52,5 +76,9 @@ class Course {
         var dateWithoutTime = month + '/' + day + '/'+ year;
         return dateWithoutTime;
       }
+
+    createHtmlElement(){
+
+    }
 }
 
