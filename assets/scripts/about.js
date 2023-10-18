043b8dc06e1b3289dcc01f3d89aa08f70ef217ba
unknown <joonasroska123@gmail.com>
2023-10-18 21:11:30 +0300

    fetch readme and show it on html is working

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -1,7 +1,12 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("About.html content loaded")
-    getGithubReadMe()
+
+    getGithubReadMe().then(respo=>{
+        inputToHtmlElement('fromGithub',respo)
+    }).catch(error =>{
+        inputToHtmlElement('fromGithub','Failed to fetch Github readme :(')
+    })
 });
 
 
@@ -10,14 +15,20 @@ async function getGithubReadMe(){
     // .then(response => response.text())
     // .then(result => {
     //     console.log(result) // Log the fetched content
-    // });  
+    // });  !!!This is here to remind me about the other way to do this
+
     try{
         const fetchedReadme = await fetch('https://raw.githubusercontent.com/JnsRdnp/deadlines/main/README.md')
         const readMeToText = await fetchedReadme.text()
-        console.log(readMeToText)
+        return readMeToText
     } catch(err){
-        console.log(err)
+        throw err
     }
-
-
 }
+
+
+async function inputToHtmlElement(elementid,text){ 
+    const element = document.getElementById(elementid)
+    element.innerHTML = text
+}
+
